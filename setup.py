713c277f6cd9311335c676860121cd3bcc270f43
diy <tsctan@foxmail.com>
2022-01-02 13:51:11 +0800

    matplotlib 版本约束放宽

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ else:
 
 setup(
     name='tsc-draw',
-    version='0.3',
+    version='0.4',
     description="绘图",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -25,7 +25,8 @@ setup(
         'Topic :: Software Development :: Libraries',
     ],
     install_requires=[
-        'matplotlib>=3.1.3,<=3.2.3',
+        # 'matplotlib>=3.1.3,<=3.2.3',
+        'matplotlib>=3.1.3',  # 雷达图形状 用不了, 但是所有电脑可以安装, 防止高版本python装不了
         'numpy>=1.18.1',
         'scipy>=1.4.1',
         'statsmodels>=0.12.2',
--- tsc_draw/__init__.py
+++ tsc_draw/__init__.py
@@ -1,7 +1,1 @@
-'''
-### 暂时无用的用于超算的文件
-- info.py
-- query.py
-- srun.sh
-'''
 from .draw import Draw
